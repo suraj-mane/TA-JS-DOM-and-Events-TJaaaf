34a8c51a3c549e704861f724575a997e2d5f3faf
suraj-mane <sun.mane1100@gmail.com>
2022-01-26 09:53:49 +0530

    complete assignment

--- block-BJaaek/code/script.js
+++ block-BJaaek/code/script.js
@@ -1,12 +1,28 @@
-let first = document.querySelector('.first');
-let second = document.querySelector('.second');
+let BoxOne = document.querySelector('.first');
+let BoxSecound = document.querySelector('.second');
 
-let color = ['red','black','yellow','green','brown'];
+function generaterandomcolor(){
+  let hexCharactors = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"];
+  let color = "#";
 
-first.addEventListener('click', function(){
-  first.style.background = 'black','green';
-});
+  for(let i=0; i < 6; i++) {
+    let randomNumber = Math.floor(Math.random() * 16);
+    color = color + hexCharactors[randomNumber];
+  }
 
-second.addEventListener('mouseover', function() {
-  second.style.background = 'red';
-});
+  return color;
+}
+
+function handleclick() {
+  let randomColor = generaterandomcolor();
+  BoxOne.style.background = randomColor;
+}
+
+function handleMouseover() {
+  let randomColor = generaterandomcolor();
+  BoxSecound.style.background = randomColor;
+}
+
+BoxOne.addEventListener('click', handleclick);
+
+BoxSecound.addEventListener('mouseover',handleMouseover);
