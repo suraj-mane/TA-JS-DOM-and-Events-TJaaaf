20670839f762aa090924a448cb4e32a2f35a7afd
suraj-mane <sun.mane1100@gmail.com>
2022-01-26 20:37:35 +0530

    complete assignment

--- block-BJaaeo/code/script.js
+++ block-BJaaeo/code/script.js
@@ -1,8 +1,28 @@
 let display = document.querySelector('.display-screen');
-let numbtn = document.querySelectorAll('.numBtn');
+let numbtn = document.querySelectorAll('li');
+
+let initialvalue = 0;
+
+display.innerText = initialvalue;
+
+
+function handl(event){
+  if(event.target.classList.contains('equal')){
+    display.innerText = eval(display.innerText);
+    return;
+  } 
+  if(event.target.classList.contains('clear')){
+    display.innerText = initialvalue;
+    return;
+  }  
+  if(display.innerText == initialvalue){
+    display.innerText = event.target.innerText;
+  } else {
+    display.innerText += event.target.innerText;
+  }
+}
+
 
 numbtn.forEach((element) => {
-  element.addEventListener('click', function(){
-     console.log(element.target.dataset-text);
-  })
-})
+  element.addEventListener('click', handl);
+  });
