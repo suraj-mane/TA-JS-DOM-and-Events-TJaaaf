a29ac86cf6bb58ae80fd97af86149785442500bd
suraj-mane <sun.mane1100@gmail.com>
2022-01-26 17:02:33 +0530

    not understand that thing

---
+++ block-BJaaep/code/script.js
@@ -0,0 +1,16 @@
+// with Delegation 
+let box = document.querySelector('.boxes');
+
+let allbox = document.querySelectorAll('.box');
+
+function getNumber() {
+  let count = 1;
+  for(i=0; i<=12; i++){
+    count =  i;
+  }
+  return count;
+}
+
+allbox.addEventListener('click', function(){
+  console.log("suraj");
+});
