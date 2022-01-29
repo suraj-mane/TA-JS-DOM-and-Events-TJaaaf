947fe9ac12e065d7382c7a2040ad866836cd6692
suraj-mane <sun.mane1100@gmail.com>
2022-01-29 11:27:38 +0530

    movie list app is done

--- block-BJaaet/code/script.js
+++ block-BJaaet/code/script.js
@@ -1,21 +1,62 @@
-let form = document.querySelector("form");
-let display = document.querySelector(".display");
+let input = document.querySelector('input[type="text"]');
+let movieRoot = document.querySelector("ul");
 
-let text = [];
+allMovies = [
+  {
+    name:"3",
+    watched: true,
+  },
+  {
+    name:"fan",
+    watched: true,
+  },
+]
 
-function addMovie(event){
-  event.preventDefault();
-  text = event.target.elements.text.value;
+input.addEventListener('keyup', (event) => {
+  if(event.keyCode === 13){
+    allMovies.push({
+      name: event.target.value,
+      watched: false,
+    });
+    event.target.value = "";
+    createMovieUI();
+  } 
+});
+
+function deleteMovie(event) {
+    let id = event.target.dataset.id;
+    allMovies.splice(id, 1);
+    createMovieUI();
+}
+
+function changeMovie(event) {
+  let id = event.target.id;
+  allMovies[id].watched = !allMovies[id].watched;
 }
 
 
-form.addEventListener("submit", addMovie);
+function createMovieUI(){
+  movieRoot.innerText = "";
+  allMovies.forEach((movie, i) => {
+    let li = document.createElement("li");
+    let input = document.createElement("input");
+    input.classList.add("check-box");
+    input.type = "checkbox";
+    input.id = i;
+    input.checked = movie.watched;
+    input.addEventListener('change', changeMovie);
+    let label = document.createElement("label");
+    label.for = "1";
+    label.classList.add("list");
+    label.innerText = movie.name;
+    let span = document.createElement("span");
+    span.innerText = "X";
+    span.setAttribute("data-id", i);
+    span.addEventListener('click', deleteMovie);
 
-text.forEach(ele => {
-  let input = document.createElement("input");
-  let span = document.createElement("span");
-  input.type = "checkbox";
-  span.innerText = "suraj";
-  input.append(span);
-  display.append(input);
-})
+    li.append(input, label, span)
+    movieRoot.append(li);
+  });
+}
+
+createMovieUI();
