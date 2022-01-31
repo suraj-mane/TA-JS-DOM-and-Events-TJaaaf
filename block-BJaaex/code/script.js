9d5ef8028479ce45ca60185c35ab37f6dc325581
suraj-mane <sun.mane1100@gmail.com>
2022-01-31 17:13:21 +0530

    complete

--- block-BJaaex/code/script.js
+++ block-BJaaex/code/script.js
@@ -1,32 +1,124 @@
-data = [
-  {
-    img:<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-    <path fillRule="evenodd" d="M14.243 5.757a6 6 0 10-.986 9.284 1 1 0 111.087 1.678A8 8 0 1118 10a3 3 0 01-4.8 2.401A4 4 0 1114 10a1 1 0 102 0c0-1.537-.586-3.07-1.757-4.243zM12 10a2 2 0 10-4 0 2 2 0 004 0z" clipRule="evenodd" />
-    </svg>,
-    name: "at-Symbol",
-  },
-  {
-    img:<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-    <path d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zM10 18a3 3 0 01-3-3h6a3 3 0 01-3 3z" />
-  </svg>,
-    name: "bell",
-  },
-]
+'use strict';
 
-let Rootgame = document.querySelector('.game');
-let btn = document.querySelector(".display");
-function symbols() {
-  data.forEach(element => {
-    let li = document.createElement("li");
-    let img = document.createElement("image");
-    img.src = element.img;
-    li.innerText = element.img;
+var cardsArray = [{
+  'name': 'shell',
+  'img': 'img/blueshell.png'
+}, {
+  'name': 'star',
+  'img': 'img/star.png'
+}, {
+  'name': 'bobomb',
+  'img': 'img/bobomb.png'
+}, {
+  'name': 'mario',
+  'img': 'img/mario.png'
+}, {
+  'name': 'luigi',
+  'img': 'img/luigi.png'
+}, {
+  'name': 'peach',
+  'img': 'img/peach.png'
+}, {
+  'name': '1up',
+  'img': 'img/1up.png'
+}, {
+  'name': 'mushroom',
+  'img': 'img/mushroom.png'
+}, {
+  'name': 'thwomp',
+  'img': 'img/thwomp.png'
+}, {
+  'name': 'bulletbill',
+  'img': 'img/bulletbill.png'
+}, {
+  'name': 'coin',
+  'img': 'img/coin.png'
+}, {
+  'name': 'goomba',
+  'img': 'img/goomba.png'
+}];
 
-    li.append(img);
-    Rootgame.append(li);
+var gameGrid = cardsArray.concat(cardsArray).sort(function () {
+  return 0.5 - Math.random();
+});
+
+var firstGuess = '';
+var secondGuess = '';
+var count = 0;
+var previousTarget = null;
+var delay = 1200;
+
+var game = document.getElementById('game');
+var grid = document.createElement('section');
+grid.setAttribute('class', 'grid');
+game.appendChild(grid);
+
+gameGrid.forEach(function (item) {
+  var name = item.name,
+      img = item.img;
+
+
+  var card = document.createElement('div');
+  card.classList.add('card');
+  card.dataset.name = name;
+
+  var front = document.createElement('div');
+  front.classList.add('front');
+
+  var back = document.createElement('div');
+  back.classList.add('back');
+  back.style.backgroundImage = 'url(' + img + ')';
+
+  grid.appendChild(card);
+  card.appendChild(front);
+  card.appendChild(back);
+});
+
+var match = function match() {
+  var selected = document.querySelectorAll('.selected');
+  selected.forEach(function (card) {
+    card.classList.add('match');
   });
-}
+};
 
-symbols();
+var resetGuesses = function resetGuesses() {
+  firstGuess = '';
+  secondGuess = '';
+  count = 0;
+  previousTarget = null;
 
-btn.addEventListener("click", symbols);
+  var selected = document.querySelectorAll('.selected');
+  selected.forEach(function (card) {
+    card.classList.remove('selected');
+  });
+};
+
+grid.addEventListener('click', function (event) {
+
+  var clicked = event.target;
+
+  if (clicked.nodeName === 'SECTION' || clicked === previousTarget || clicked.parentNode.classList.contains('selected') || clicked.parentNode.classList.contains('match')) {
+    return;
+  }
+
+  if (count < 2) {
+    count++;
+    if (count === 1) {
+      firstGuess = clicked.parentNode.dataset.name;
+      console.log(firstGuess);
+      clicked.parentNode.classList.add('selected');
+    } else {
+      secondGuess = clicked.parentNode.dataset.name;
+      console.log(secondGuess);
+      clicked.parentNode.classList.add('selected');
+    }
+
+    if (firstGuess && secondGuess) {
+      if (firstGuess === secondGuess) {
+        setTimeout(match, delay);
+      }
+      setTimeout(resetGuesses, delay);
+    }
+    previousTarget = clicked;
+  }
+});
