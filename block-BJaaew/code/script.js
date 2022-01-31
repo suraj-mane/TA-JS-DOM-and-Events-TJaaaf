ecb6b757aad12e5c0bdf0f47501805433bfd3c8a
suraj-mane <sun.mane1100@gmail.com>
2022-01-31 15:22:34 +0530

    all things is complete

--- block-BJaaew/code/script.js
+++ block-BJaaew/code/script.js
@@ -1,27 +1,19 @@
 let Rootelement = document.querySelector(".display");
 let body = document.querySelector('body');
 let search = document.querySelector('.search');
-let stark = document.querySelector('.btn-stark');
-let lannister = document.querySelector('.btn-lannister');
-let barathon = document.querySelector('.btn-barathon');
-let targaryen = document.querySelector('.btn-targaryen');
-let greyjoy = document.querySelector('.btn-greyjoy');
-let tyrell = document.querySelector('.btn-tyrell');
-let tully = document.querySelector('.btn-tully');
-let redwyne = document.querySelector('.btn-redwyne');
-let frey = document.querySelector('.btn-frey');
-let arryn = document.querySelector('.btn-arryn');
-let dothraki = document.querySelector('.btn-dothraki');
+let rootTag = document.querySelector('.tag');
 
+let allTag = got.houses.map((house) => house.name); 
 
-let activeBtn = "";
+let activeHouse = "";
 
 let allpeople = got.houses.reduce((acc, cv) => {
   acc = acc.concat(cv.people);
   return acc;
 }, []);
 
-function displayInfo(data = allpeople) {
+function displayInfo(data = []) {
+  Rootelement.innerText = "";
   data.forEach((info) => {
     let li = document.createElement("li");
     li.classList.add("box");
@@ -42,129 +34,36 @@ function displayInfo(data = allpeople) {
   });
 }
 
-displayInfo();
+displayInfo(allpeople);
 
+function createTagsUI(tags = []){
+  rootTag.innerText = "";
+  tags.forEach((tag) => {
+    let li = document.createElement("li");
+    li.innerText = tag; 
+    li.classList.add("btn");
+    if(activeHouse === tag){
+      li.classList.add("active");
+    }
 
-search.addEventListener("keyup" , (event) => {
-  let filterName = allpeople.filter((data) =>
-    data.name.includes(event.target.value)
-  );
-  displayInfo(filterName);
-});
-
-stark.addEventListener('click', () => {
-  let starksName = allpeople.filter((data) => data.name.includes("Stark"));
-  console.log(starksName);
-  activeBtn = "stark";
-  updateActive();
-});
-
-lannister.addEventListener('click', () => {
-  let lannistersName = allpeople.filter((data) => data.name.includes("Lannister"));
-  console.log(lannistersName);
-  activeBtn = "lannister";
-  updateActive();
-});
-
-barathon.addEventListener('click', () => {
-  let barathon = allpeople.filter((data) => data.name.includes("Baratheon"));
-  console.log(barathon);
-  activeBtn = "barathon";
-  updateActive();
-});
-
-targaryen.addEventListener('click', () => {
-  let targaryen = allpeople.filter((data) => data.name.includes("Targaryen"));
-  console.log(targaryen);
-  activeBtn = "targaryen";
-  updateActive();
-});
-
-greyjoy.addEventListener('click', () => {
-  let greyjoy = allpeople.filter((data) => data.name.includes("Greyjoy"));
-  console.log(greyjoy);
-  activeBtn = "greyjoy";
-  updateActive();
-});
-
-tyrell.addEventListener('click', () => {
-  let tyrell = allpeople.filter((data) => data.name.includes("Tyrell"));
-  console.log(tyrell);
-  activeBtn = "tyrell";
-  updateActive();
-});
-
-tully.addEventListener('click', () => {
-  let tully = allpeople.filter((data) => data.name.includes("Tully"));
-  console.log(tully);
-  activeBtn = "tully";
-  updateActive();
-});
-
-redwyne.addEventListener('click', () => {
-  let redwyne = allpeople.filter((data) => data.name.includes("Redwyne"));
-  console.log(redwyne);
-  activeBtn = "redwyne";
-  updateActive();
-});
-
-frey.addEventListener('click', () => {
-  let frey = allpeople.filter((data) => data.name.includes("Frey"));
-  console.log(frey);
-  activeBtn = "frey";
-  updateActive();
-});
-
-arryn.addEventListener('click', () => {
-  let arryn = allpeople.filter((data) => data.name.includes("Arryn"));
-  console.log(arryn);
-  activeBtn = "arryn";
-  updateActive();
-});
-
-dothraki.addEventListener('click', () => {
-  let dothraki = allpeople.filter((data) => data.name.includes("Drogo"));
-  console.log(dothraki);
-  activeBtn = "dothraki";
-  updateActive();
-});
-
-function updateActive(btn = activeBtn) {
-  stark.classList.remove("active");
-  lannister.classList.remove("active");
-  barathon.classList.remove("active");
-  targaryen.classList.remove("active");
-  greyjoy.classList.remove("active");
-  tyrell.classList.remove("active");
-  tully.classList.remove("active");
-  redwyne.classList.remove("active");
-  frey.classList.remove("active");
-  arryn.classList.remove("active");
-  dothraki.classList.remove("active");
-  if(btn === "stark"){
-    stark.classList.add("active");
-  } else if( btn === "lannister"){
-    lannister.classList.add("active");
-  } else if( btn === "barathon"){
-    barathon.classList.add("active");
-  } else if( btn === "targaryen"){
-    targaryen.classList.add("active");
-  } else if( btn === "greyjoy"){
-    greyjoy.classList.add("active");
-  } else if( btn === "tyrell"){
-    tyrell.classList.add("active");
-  } else if( btn ==="tully"){
-    tully.classList.add("active");
-  } else if( btn === "redwyne"){
-    redwyne.classList.add("active");
-  } else if( btn === "frey"){
-    frey.classList.add("active");
-  } else if( btn === "arryn"){
-    arryn.classList.add("active");
-  } else if( btn === "dothraki"){
-    dothraki.classList.add("active");
-  }
+    li.addEventListener('click', () => {
+      activeHouse = tag;
+      let peopleOfTheHouse = got.houses.find((house) => house.name === tag).people || [];
+      displayInfo(peopleOfTheHouse);
+      createTagsUI(allTag);
+    }) 
+    rootTag.append(li);
+  });
 }
 
+createTagsUI(allTag);
+
+function handle(event) {
+  serchText = event.target.value;
+  let filterPeople  = allpeople.filter((p) => p.name.toLowerCase().includes(serchText.toLowerCase()));
+  displayInfo(filterPeople);
+}
+
+search.addEventListener("input", handle);
 body.addEventListener("load" , displayInfo);
 
